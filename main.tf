db300c8f515987861b07eee507f5f736f48c8088
PRATEEKSHAGK-05 <gkprateeksha@gmail.com>
2026-02-13 12:03:18 +0530

    gave count

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "aws_instance" "dev_server" {
-
+  count=5
   ami           = var.ami_id
   instance_type = var.instance_type
 
   user_data = file("userdata.sh")
 
   tags = {
-    Name = "webserver-terraform"
+    Name = "server"
   }
 
 }
+
