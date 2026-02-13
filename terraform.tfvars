7553a4892d28d09a7b15a66e667dfb918a85122b
PRATEEKSHAGK-05 <gkprateeksha@gmail.com>
2026-02-13 11:35:15 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+region="ap-south-2"
+instance_type="t3.micro"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "ap-south-2"
+}
+variable "instance_type"{
+  default="t3.micro"
+} 
+variable "ami_id"{
+  default="ami-01cfb0266fc955899"
+} 
